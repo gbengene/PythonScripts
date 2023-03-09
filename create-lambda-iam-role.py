32fb4c3c54841773c02acb02319d500b3c768493
gbemigaogele <GbemigaOgele@gmail.com>
2023-03-09 08:00:25 -0500

    Update create-lambda-iam-role.py

--- create-lambda-iam-role.py
+++ create-lambda-iam-role.py
@@ -1,31 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@gbengene 
-dahjohnson
-/
-Boto3
-Public
-Fork your own copy of dahjohnson/Boto3
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-Boto3/api-gateway-lambda-sqs-deployment/create-lambda-iam-role.py /
-@dahjohnson
-dahjohnson added api-gateway-lambda-sqs-deployment project directory
-Latest commit 11ff762 last month
- History
- 1 contributor
-61 lines (52 sloc)  1.43 KB
 
 import boto3
 import json
@@ -88,18 +60,4 @@ attach_policy = iam.attach_role_policy(
 role_create_date = lambda_role['Role']['CreateDate']
 
 print(f"The Lambda Execution Role \"{lambda_role_name}\" was created on \"{role_create_date}.\"")
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
- You signed in with another tab or window. Reload to refresh your session.Boto3/create-lambda-iam-role.py at main · dahjohnson/Boto3
+
